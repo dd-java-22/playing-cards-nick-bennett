dd6550253fb086d180690ad570f3f2ed7e1f52b3
nick-bennett <nbennett10@cnm.edu>
2026-02-02 15:14:18 -0700

    Custom comparators moved to lambdas.

--- src/main/java/edu/cnm/deepdive/cards/service/Trick.java
+++ src/main/java/edu/cnm/deepdive/cards/service/Trick.java
@@ -4,7 +4,6 @@ import edu.cnm.deepdive.cards.model.Card;
 import edu.cnm.deepdive.cards.model.Deck;
 import edu.cnm.deepdive.cards.model.Suit.Color;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.random.RandomGenerator;
 
@@ -47,25 +46,19 @@ public class Trick {
       }
     }
 
-    blackPile.sort(new Comparator<>() {
-      @Override
-      public int compare(Card card1, Card card2) {
-        int result = card1.getColor().compareTo(card2.getColor());
-        if (result == 0) {
-          result = card1.compareTo(card2);
-        }
-        return result;
+    blackPile.sort((card1, card2) -> {
+      int result = card1.getColor().compareTo(card2.getColor());
+      if (result == 0) {
+        result = card1.compareTo(card2);
       }
+      return result;
     });
-    redPile.sort(new Comparator<>() {
-      @Override
-      public int compare(Card card1, Card card2) {
-        int result = card2.getColor().compareTo(card1.getColor());
-        if (result == 0) {
-          result = card1.compareTo(card2);
-        }
-        return result;
+    redPile.sort((card1, card2) -> {
+      int result = card2.getColor().compareTo(card1.getColor());
+      if (result == 0) {
+        result = card1.compareTo(card2);
       }
+      return result;
     });
   }
 
